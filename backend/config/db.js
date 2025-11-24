aec8e30409abc971975917a438775a10128f2b98
Favionetah <faviosandy30@gmail.com>
2025-11-23 23:27:57 -0400

    Cambien La Base de Datos xD

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'cambienle',
+    password: 'root',
     database: 'pizzeria_db',
     waitForConnections: true,
     connectionLimit: 10,
