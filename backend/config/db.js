1d8384ae5fa8274b4bfb3b6969176a6432918c49
Martin Tejeda <martintejeda157@gmail.com>
2025-11-24 00:15:21 -0400

    validacion register

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,11 +8,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-<<<<<<< HEAD
-    password: 'root',
-=======
     password: 'MTG13',
->>>>>>> 66e8887750b5157c2a1b5380a0abf79ba8539c26
     database: 'pizzeria_db',
     waitForConnections: true,
     connectionLimit: 10,
